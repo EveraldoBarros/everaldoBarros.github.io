def5dda1e170942301ce6adbf7c551735649e84f
EveraldoBarros <everaldobarros.facul2021@gmail.com>
2024-12-02 21:38:38 -0300

    Portfólio modificado para atender os requisitos do trabalho universitário.

---
+++ java-script/script.js
@@ -0,0 +1,14 @@
+// Adiciona um alerta de sucesso ao enviar o formulário
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contact-form");
+  
+    if (form) {
+      form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Impede o envio padrão
+        alert("Formulário enviado com sucesso!");
+        form.reset(); // Reseta o formulário
+      });
+    }
+  });
+  
+  
